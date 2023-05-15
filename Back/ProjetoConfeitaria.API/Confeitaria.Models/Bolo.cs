6d5584561e25f972c8960e90ac325a528872bc4d
Vitor Amorim <vitor232596@hotmail.com>
2023-05-15 18:31:10 -0300

    Adicionando Classes

---
+++ Back/ProjetoConfeitaria.API/Confeitaria.Models/Bolo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Confeitaria.Models
+{
+    public class Bolo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Ingredientes { get; set; }
+        public double preco { get; set; }
+
+    }
+}
---
+++ Back/ProjetoConfeitaria.API/Confeitaria.Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Confeitaria.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string telefone { get; set; }
+        public ICollection<Endereco> Enderecos { get; set; }
+    }
+}
